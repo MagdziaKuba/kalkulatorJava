b9a63f61b38b159629818e8e8e62d612a3778ed6
Magda <mkubacomperia@gmail.com>
2022-02-18 13:46:33 +0100

    przeciążenie metod

---
+++ src/MethodName.java
@@ -0,0 +1,13 @@
+public class MethodName {
+    //przeciążenie metod, różne parametry
+    public void add(int a, int b){
+        System.out.println(a+b);
+    }
+
+    public void add(int a, int b, int c){
+        System.out.println(a+b+c);
+    }
+    public void add(String a, String b){
+        System.out.println(a+b);
+    }
+}
---
+++ src/MethodNameTest.java
@@ -0,0 +1,9 @@
+public class MethodNameTest {
+    public static void main(String[] args) {
+        MethodName methodname=new MethodName();
+        methodname.add(2,3);
+        methodname.add(2,3,1);
+        methodname.add("2","3");
+
+    }
+}
