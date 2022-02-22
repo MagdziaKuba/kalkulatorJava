e9c9b4450d583660140df61f6de15f854fc6bab5
Magda <mkubacomperia@gmail.com>
2022-02-22 10:30:45 +0100

    zbiory

---
+++ src/sety/SetyTest.java
@@ -0,0 +1,25 @@
+package sety;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetyTest {
+    public static void main(String[] args) {
+        //zbiór
+        Set<String> fruits=new HashSet<>();
+        fruits.add("apple");
+        fruits.add("orange");
+        fruits.add("orange");//w zbiorach nie istnieją duplikaty
+
+        System.out.println(fruits.contains("apple"));
+        System.out.println(fruits.size());
+        System.out.println(fruits.isEmpty());
+
+        Set<String> vege=new HashSet<>();
+        vege.add("potatoes");
+        vege.addAll(fruits);
+        System.out.println(vege.size());
+
+
+    }
+}
